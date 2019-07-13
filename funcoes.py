c5e7858e6ff302fc18659ab2250a568706ec5053
Lucas <lucasdevpython@gmail.com>
2019-07-13 16:22:36 -0300

    Conexão separada excluída, adicionado seleção pelo ID

--- funcoes.py
+++ funcoes.py
@@ -1,17 +1,41 @@
-from pydb import *
+import pymysql.cursors
 
 def index():
     while True:
         print('"Listagem de comandos"')
         print('exit = sair da aplicação')
         print('MD = Mostrar todos os dados da tabela')
+        print('ID = Mostra os dados de um ID')
         cmd = str(input('Digite o comando desejado: ')).strip().lower()
         if cmd =='exit':
             break
         elif cmd =='md':
             mostrarDados()
-
+        elif cmd =='id':
+            a = int(input('Digite o ID que deseja ver: '))
+            selecioneId(a)
 
 
 def mostrarDados():
-    conn()
+    connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='130217',
+                             db='crud',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM pessoas")
+    result = cursor.fetchall()
+    print(result)
+
+def selecioneId(id):
+    connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='130217',
+                             db='crud',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM pessoas WHERE id = {id}")
+    result = cursor.fetchall()
+    print(result)
