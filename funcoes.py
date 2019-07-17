fbda2d3f3dca735e8187c95ea71a66f003c0a541
Lucas <lucasdevpython@gmail.com>
2019-07-16 22:30:21 -0300

    Atualizada conexão na funções e alterado MD, está com erro ao mostrar os valores de dict.

--- funcoes.py
+++ funcoes.py
@@ -1,12 +1,24 @@
 import pymysql.cursors
 
+connection = pymysql.connect(host='localhost',
+                        user='root',
+                        password='130217',
+                        db='crud',
+                        charset='utf8mb4',
+                        cursorclass=pymysql.cursors.DictCursor)
+
+cursor = connection.cursor()
+
+
 def index():
     while True:
+        print('-'*40)
         print('"Listagem de comandos"')
         print('exit = sair da aplicação')
         print('MD = Mostrar todos os dados da tabela')
         print('ID = Mostra os dados de um ID')
         print('ADD = Inserir novos dados')
+        print('-'*40)
         cmd = str(input('Digite o comando desejado: ')).strip().lower()
         if cmd =='exit':
             break
@@ -23,39 +35,25 @@ def index():
 
 
 def mostrarDados():
-    connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='130217',
-                             db='crud',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-    cursor = connection.cursor()
     cursor.execute("SELECT * FROM pessoas")
     result = cursor.fetchall()
-    print(result)
+    resultChaves = result[0]
+    for k, v in resultChaves.items():
+        print(k, end='  ')
+    print()
 
 def selecioneId(id):
-    connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='130217',
-                             db='crud',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
     cursor = connection.cursor()
     cursor.execute(f"SELECT * FROM pessoas WHERE id = {id}")
     result = cursor.fetchall()
     print(result)
 
 def inserirDB(nome,email,senha):
-    connection = pymysql.connect(host='localhost',
-                                 user='root',
-                                 password='130217',
-                                 db='crud',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
     cursor = connection.cursor()
     sql = f"INSERT INTO pessoas (nome, email, senha) VALUES ('{nome}', '{email}', '{senha}')"
-#    print(f'"{sql}"')
     cursor.execute(sql)
     connection.commit()
     connection.close()
+
+
+#https://pt.stackoverflow.com/questions/308346/como-imprimir-as-informa%C3%A7%C3%B5es-no-formato-de-tabela-em-python
--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 from funcoes import *
 
-print('Bem-vindo ao CRUD')
+print('='*40)
+print(f'Bem-vindo ao CRUD')
+print('='*40)
 index()
